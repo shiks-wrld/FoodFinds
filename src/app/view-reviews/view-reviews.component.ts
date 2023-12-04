81941a5f7ddbfab6be881a7eb57d48777dc4d229
shiks-wrld <ssikliga@umich.edu>
2023-12-04 01:59:53 -0500

    View review update

--- src/app/view-reviews/view-reviews.component.ts
+++ src/app/view-reviews/view-reviews.component.ts
@@ -7,29 +7,18 @@ import {MatChipsModule} from "@angular/material/chips";
 import {FormBuilder, FormsModule} from "@angular/forms";
 import {ReviewService} from "../services/ReviewService/review.service";
 import {MatInputModule} from "@angular/material/input";
-import {Options} from "../models/options.model";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-view-reviews',
   standalone: true,
-  imports: [CommonModule, FlexModule, MatButtonModule, MatCardModule, MatChipsModule, MatInputModule, FormsModule],
+  imports: [CommonModule, FlexModule, MatButtonModule, MatCardModule, MatChipsModule, MatInputModule, FormsModule, MatIconModule],
   templateUrl: './view-reviews.component.html',
   styleUrl: './view-reviews.component.css'
 })
 export class ViewReviewsComponent implements OnInit {
   reviews: any;
   gridColumns = 3;
-
-  cuisinesOpts: Options[] = [
-    {value: 'Italian', viewValue: 'Italian'},
-    {value: 'Mexican', viewValue: 'Mexican'},
-    {value: 'Chinese', viewValue: 'Chinese'},
-    {value: 'Indian', viewValue: 'Indian'},
-    {value: 'American', viewValue: 'American'},
-    {value: 'Fast-Food', viewValue: 'Fast Food'},
-    {value: 'Japanese', viewValue: 'Japanese'},
-    {value: 'Vegan', viewValue: 'Vegan'},
-  ];
 
   constructor(private fb: FormBuilder, private reviewService: ReviewService) {}
 
